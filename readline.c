d570acff3e497130233aab7dfa5c045fb263e321
tommydebisi <tommydebisi@gmail.com>
2022-06-14 16:20:16 +0100

    :art: modified readline

--- readline.c
+++ readline.c
@@ -6,4 +6,17 @@
  * @fd: file descriptor of file to be read
  * Return: 1 if no error else -1 if error
  */
-int readline
+int readline(int fd)
+{
+    int get_check;
+    ssize_t bufzize = 0;
+    char *buf = NULL;
+
+    while (1)
+    {
+        get_check = getline(&buf, &bufzize, fd);
+        if (get_check == -1)
+            break;
+    }
+    return (1);
+}
