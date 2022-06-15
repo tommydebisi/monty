e86914da18051da92e4f232edd88e8f739e46826
odelolajosh <oladapoodelola@gmail.com>
2022-06-15 14:57:16 +0100

    :construction: add declarations for new opcode impl

--- monty.h
+++ monty.h
@@ -68,6 +68,11 @@ void pop(stack_t **stack, unsigned int nline);
 void swap(stack_t **stack, unsigned int nline);
 void add(stack_t **stack, unsigned int nline);
 void nop(stack_t **stack, unsigned int nline);
+void mod(stack_t **stack, unsigned int nline);
+void pchar(stack_t **stack, unsigned int nline);
+void pstr(stack_t **stack, unsigned int nline);
+void rotl(stack_t **stack, unsigned int nline);
+void rotr(stack_t **stack, unsigned int nline);
 
 void (*get_ops(char *opcode))(stack_t **stack, unsigned int nline);
 
